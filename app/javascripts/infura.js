bfa6cac0f38111fe9284ec2912b480dcd1499fb8
tanglong <1010851542@qq.com>
2018-06-26 10:34:29 +0800

    Update infura.js

--- app/javascripts/infura.js
+++ app/javascripts/infura.js
@@ -3,15 +3,15 @@ var web3  = new web3();
 var web3Admin = require("web3admin");
 web3Admin.extend(web3);//扩展web3模块
 // xxxx为你在infura申请的地址
-web3.setProvider(new web3.providers.HttpProvider("https://ropsten.infura.io/bhkRk7IJ023nMWaG09ov"));
-/*web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/bhkRk7IJ023nMWaG09ov"));*/
+web3.setProvider(new web3.providers.HttpProvider("XX"));
+/*web3 = new Web3(new Web3.providers.HttpProvider("XX"));*/
 
 // wei是以太坊上的的最小单位，ether小数点后18位为一个wei
-//var balanceWei = web3.eth.getBalance("0xc4969Fd4ec16f1EA57CDB928455C2Af3347403C7").toNumber();
+//var balanceWei = web3.eth.getBalance("XX").toNumber();
 // 从wei转换成ether
 //var balance = web3.fromWei(balanceWei, 'ether');
 
-//web3.eth.personal.unlockAccount('0x9382b62F56A940aB85aEc37DFfaa3CdCA2b7eF3d', "long150700", 5);
+//web3.eth.personal.unlockAccount('XX', "long150700", 5);
 //console.log(balance);
 var MyTokenABI = [
     {
@@ -189,16 +189,16 @@ var MyTokenABI = [
     }
 ];
 
-var aAddress = "0xFEf695E315c68a7CaB26D0eEF11765544DBB0E6f";
+var aAddress = "XX";
 // 目前web3没有api来解锁账户,只能自己事先解锁
 var tokenContract = web3.eth.contract(MyTokenABI, null, {
-    from: '0x1a9ec3b0b807464e6d3398a59d6b0a369bf422fa'
+    from: 'XX'
 });
 
 //获取协议
-//var tokenContracted = web3.eth.contract(MyTokenABI, '0xFEf695E315c68a7CaB26D0eEF11765544DBB0E6f');
+//var tokenContracted = web3.eth.contract(MyTokenABI, 'XX');
 
-/*tokenContracted.methods.transfer("0x8c1b2e9e838e2bf510ec7ff49cc607b718ce8401",387).send({from: '0x1a9ec3b0b807464e6d3398a59d6b0a369bf422fa'})
+/*tokenContracted.methods.transfer("XX",387).send({from: 'XX'})
     .on('transactionHash', function(hash){
     })
     .on('confirmation', function(confirmationNumber, receipt){
@@ -213,7 +213,7 @@ var a = web3.eth.contract(MyTokenABI).at(aAddress);
 
 //var abc = a.constructor;
 //console.log(abc);
-var banlances = a.balanceOf("0xc4969Fd4ec16f1EA57CDB928455C2Af3347403C7");
+var banlances = a.balanceOf("XX");
 console.log(banlances);
 
 
